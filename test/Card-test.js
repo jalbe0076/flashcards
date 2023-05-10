cff63bf242292008ba9185b48ced8781a4774055
Jason Alberto <alberto.jason@gmail.com>
2023-05-09 20:14:50 -0400

    Add round test to create an object

--- test/Card-test.js
+++ test/Card-test.js
@@ -43,8 +43,22 @@ describe('deck', () => {
     const card2 = createCard(sampleData[1].id, sampleData[1].question, sampleData[1].answers, sampleData[1].correctAnswer);
     const card3 = createCard(sampleData[2].id, sampleData[2].question, sampleData[2].answers, sampleData[2].correctAnswer);
 
-    const deck = createDeck([card1, card2, card3])
+    const deck = createDeck([card1, card2, card3]);
 
     expect(deck).to.have.lengthOf(3);
+  });
+});
+
+describe('round', () => {
+  it('should be an object that holds onto the deck', () => {
+    const card1 = createCard(sampleData[0].id, sampleData[0].question, sampleData[0].answers, sampleData[0].correctAnswer);
+    const card2 = createCard(sampleData[1].id, sampleData[1].question, sampleData[1].answers, sampleData[1].correctAnswer);
+    const card3 = createCard(sampleData[2].id, sampleData[2].question, sampleData[2].answers, sampleData[2].correctAnswer);
+    
+    const deck = createDeck([card1, card2, card3]);
+
+    const round = createRound(deck);
+
+    expect(round.deck).to.deep.equal(deck);
   });
 });
